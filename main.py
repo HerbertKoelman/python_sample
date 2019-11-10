bb5b3047501508586e334e7b83694d2012cdffb7
herbert koelman <herbert.koelman@me.com>
2019-11-10 18:16:51 +0100

    Features that could help install/deploy artifacts into developper's build env.

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import utils
 
 if __name__ == '__main__':  # nécessaire que si on veut eviter que ce code soit systématiquement exécuté. Que ce soit comme import ou programme
-    print("Hello, python world...")
+
+    print(">>>>>>>>>>>>>>>><  WITH FUNCTIONS\n")
+
     utils.deploy.artifact('boogla-1.0.0', here='/deploy/here')
     utils.deploy.check_archive_integrity('boolga-1.0.0-x86_64.tar.gz')
 
-    boogla = utils.Artifact('boogla-1.2.3')
-    print ("Artefact: " , boogla, "Id: ", boogla.id())
+    print(">>>>>>>>>>>>>>>><  WITH CLASSES\n")
 
-    boogla_package = utils.Package(boogla)
-    common_package = utils.Package(utils.Artifact('common', version='2.2.1'))
-    print (boogla_package)
-    print (common_package)
+    ipcm = utils.Artifact('ipcm-1.2.3-snapshot', description="""
+This is a very cool thing, crafted by a genius.
+    
+"Le monde sommeille par manque d'imprudence"
+    """)
+    print(ipcm.summary())
+    utils.PACKAGES.add(ipcm)
 
-    PACKAGES_HOME='/share/modules'
-    packages = utils.Packages(PACKAGES_HOME)
-    packages.search_for(boogla_package)
+    utils.PACKAGES.search_for(utils.Artifact('boogla-1.2.3'))
+    utils.PACKAGES.search_for(utils.Artifact('common', version='2.2.1'))
+    utils.PACKAGES.search_for(ipcm)
+
+
+    utils.PACKAGES.deploy(utils.Artifact('common', version='2.2.1'), '/deploy/here')
--- utils/artifacts.py
+++ utils/artifacts.py
@@ -1,33 +1,65 @@
 import platform
+import utils.deploy
+
+class Package:
+    pass
 
 class Artifact:
+    """
+    Un truc fait par un dévéloppeur.
 
-    def __init__(self, name: str, build_type: str = "Release", version = None, arch: str = None, description: str = None):
+    En informatique, un artéfact computationnel est le résultat obtenu par l'homme par l'usage d'outils ou de principes
+    reliés aux domaines de l'informatique, du multimédia ou de la pensée computationnelle. Un artéfact computationnel
+    peut être un programme, un script, un microprocesseur, une image, un jeu, une vidéo, une page Web, etc.
+    """
+
+    def __init__(self, name: str, build_type: str = None, version = None, os: str = None, target_arch: str = None, description: str = None):
 
         self.version = version
+        self.build_type = build_type
+        self.os = os
+
         tokens = name.split('-')
 
         if len(tokens) == 2:
             self.name = tokens[0]
             self.version = tokens[1]
+        elif len(tokens) == 3:
+            self.name = tokens[0]
+            self.version = tokens[1]
+            self.build_type=tokens[2]
+        elif len(tokens) == 4:
+            self.name = tokens[0]
+            self.os = tokens[1]
+            self.version = tokens[3]
+            self.build_type=tokens[4]
         else:
             self.name = name
 
-        self.build_type = build_type
         self.description = description
 
-        if arch is None:
-            self.arch = platform.machine()
+        if target_arch is None:
+            self.target_arch = platform.machine()
         else:
-            self.arch = arch
+            self.target_arch = target_arch
+
+        self.package = Package(self)
 
         assert self.version is not None, "missing Artefact version parameter, failed to initialize Artefact {}".format(name)
 
     def id(self):
-        return "{name}-{version}-{arch}".format(name=self.name, version=self.version, arch=self.arch)
+        return "{name}-{version}-{arch}".format(name=self.name, version=self.version, arch=self.target_arch)
+
+    def summary(self):
+        print("name: ", self.name)
+        print("OS: ", self.os)
+        print("arch: ", self.target_arch)
+        print("version: {} ({})".format(self.version, self.build_type))
+        print("description --------------------------------------------------------")
+        print(self.description)
 
     def __str__(self):
-        return "{name} (version: {version}, arch: {arch}, build type: {build_type})".format(version=self.version, name=self.name, arch= self.arch, build_type=self.build_type)
+        return "{name} (version: {version}, arch: {arch}, build type: {build_type})".format(version=self.version, name=self.name, arch= self.target_arch, build_type=self.build_type)
 
 class Package:
     """
@@ -58,5 +90,19 @@ class Packages:
 
         self.package_index = {}
 
-    def search_for(self, package: Package):
-        print("Search for package: {}\nFull information\n{}".format(package.artifact.name, package))
+    def add(self, artifact: Artifact):
+        self.package_index[artifact.id()] = artifact
+
+    def deploy(self, artifact: Artifact, here):
+        utils.deploy.check_archive_integrity(artifact.package.archive)
+
+    def search_for(self, item):
+        if isinstance(item, Package):
+            print("Search for package: {}\nFull information\n{}".format(item.artifact.name, item))
+        if isinstance(item, Artifact):
+            print("Search for package: {}\nFull information\n{}".format(item.name, item.package))
+
+
+# singleton
+PACKAGES_HOME = '/share/modules'
+PACKAGES = Packages(PACKAGES_HOME)
