17469338e539a3e7c67f1f18697a523ba049aa79
herbert koelman <herbert.koelman@me.com>
2019-11-19 19:48:43 +0100

    refs #5
    - copy reached 78%
    - deploy reached 74%

--- tests/apps_unit_tests.py
+++ tests/apps_unit_tests.py
@@ -1,4 +1,5 @@
 import unittest
+import sys
 import artifacts
 import artifacts.deploy_artifacts_app
 import artifacts.copy_artifacts_app
@@ -17,13 +18,41 @@ class deploy_app_test_cases(unittest.TestCase):
         shutil.rmtree(self.workspace, ignore_errors=True)
         shutil.rmtree(self.repository, ignore_errors=True)
 
-    def test_deploy_app(self):
+    def test_deploy_app_help(self):
         with self.assertRaises(SystemExit):
+            sys.argv = [
+                "{}.copy_app_test".format(__name__),
+                '-h'
+            ]
+
             artifacts.deploy_artifacts_app.main()
 
-    def test_copy_app(self):
+    def test_deploy_app(self):
+        self.test_copy_app()
+        sys.argv = [
+            "{}.copy_app_test".format(__name__),
+            '--packages-home', self.repository,
+            '--target-arch', 'x86',
+            '--install-dir', self.workspace,
+            'tests/dependencies.yml'
+        ]
+        artifacts.deploy_artifacts_app.main()
+
+    def test_copy_app_help(self):
         with self.assertRaises(SystemExit):
+            sys.argv = [
+                "{}.copy_app_test".format(__name__),
+                '-h'
+            ]
             artifacts.copy_artifacts_app.main()
+
+    def test_copy_app(self):
+        sys.argv = [
+            "{}.copy_app_test".format(__name__),
+            '--packages-home', self.repository,
+            'tests/fixtures/'
+        ]
+        artifacts.copy_artifacts_app.main()
 
 if __name__ == '__main__':
     unittest.main()
