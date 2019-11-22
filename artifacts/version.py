09f6cbe6e6cfc1e222747258cb3ab51c8577e8a3
herbert koelman <herbert.koelman@me.com>
2019-11-22 19:44:07 +0100

    setup next coming version info

--- artifacts/version.py
+++ artifacts/version.py
@@ -3,4 +3,4 @@
 import semantic_version
 
 """artifacts' package version"""
-__version__= semantic_version.Version(major=0, minor=2, patch=0)
+__version__= semantic_version.Version(major=0, minor=3, patch=0), prerelease=('snapshot', '0')
